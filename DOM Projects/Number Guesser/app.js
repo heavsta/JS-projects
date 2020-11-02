0854f8289827c1bb865f187a0617d8f90ca9de40
heavsta <francoisleduc@ymail.com>
2020-11-02 15:11:34 +0100

    losing case done, refacturing of code done through the gameOver function

--- DOM Projects/Number Guesser/app.js
+++ DOM Projects/Number Guesser/app.js
@@ -27,14 +27,44 @@ UIguessBtn.addEventListener('click', function() {
 
     // Check if won
     if(guess === winningNum) {
-        // Disable input
-        UIguessInput.disabled = true;
-        // Change border color
-        UIguessInput.style.borderColor = 'green';
-        // Set message
-        setMessage(`${winningNum} is correct, You Win!`, 'green');
+        // Game Over - WON
+        gameOver(true, `${winningNum} is correct, You Win!`)
+
+    } else {
+        // Wrong number
+        guessesLeft -= 1;
+
+        if(guessesLeft === 0) {
+            //Game Over - LOST
+            gameOver(false, `Game Over, You Lost! The correct number was ${winningNum}`)
+
+        } else {
+            // Game keeps on - answer wrong
+
+            // Change border color
+            UIguessInput.style.borderColor = 'red';
+
+            // Clear Input
+            UIguessInput.value = '';
+
+            // Tell user it is the wrong number
+            setMessage(`${guess} is not correct, ${guessesLeft} guess(es) left`, 'red');
+        }
     }
 });
+
+// Game over
+function gameOver(won, msg) {
+    let color;
+    won === true ? color = 'green' : color = 'red';
+
+    // Disable input
+    UIguessInput.disabled = true;
+    // Change border color
+    UIguessInput.style.borderColor = color;
+    // Set message
+    setMessage(msg, color);
+}
 
 // Set message
 function setMessage(msg, color){

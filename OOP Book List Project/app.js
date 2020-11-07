9e917d053d43a586dedacfa8c0071bfc417f6ecd
heavsta <francoisleduc@ymail.com>
2020-11-07 19:44:21 +0100

    Validation & Alert popup added

--- OOP Book List Project/app.js
+++ OOP Book List Project/app.js
@@ -25,12 +25,33 @@ UI.prototype.addBookToList = function(book){
     list.appendChild(row);
 }
 
+// Show Alert
+UI.prototype.showAlert = function(message, className) {
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    // Insert alert
+    container.insertBefore(div, form);
+
+    // Timeout 
+    setTimeout(function() {
+        document.querySelector('.alert').remove();
+    }, 2500)
+}
+
 // Clear Fiels
 UI.prototype.clearFields = function() {
     document.getElementById('title').value = '';
     document.getElementById('author').value = '';
     document.getElementById('isbn').value = '';
 }
+
 // Event Listeners
 document.getElementById('book-form').addEventListener('submit', function(e) {
     // Get form values
@@ -43,12 +64,21 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
 
     // Instantiate UI
     const ui = new UI();
-    
-    // Add book to list
-    ui.addBookToList(book);
 
-    // Clear Fields
-    ui.clearFields();
+    // Validate
+    if(title === '' || author === '' || isbn ==='') {
+        // Error alert
+        ui.showAlert('Please fill in all fields', 'error');
+    } else {
+        // Add book to list
+        ui.addBookToList(book);
+
+        // Show success
+        ui.showAlert('Book Added!', 'success')
+
+        // Clear Fields
+        ui.clearFields();
+    }
 
     e.preventDefault(); 
 });

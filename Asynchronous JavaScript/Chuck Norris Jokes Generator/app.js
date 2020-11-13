1329e6b3299cfd5b9a6602ffeb4a51317cb8a9b3
heavsta <francoisleduc@ymail.com>
2020-11-13 16:47:15 +0100

    rendering on the UI done

--- Asynchronous JavaScript/Chuck Norris Jokes Generator/app.js
+++ Asynchronous JavaScript/Chuck Norris Jokes Generator/app.js
@@ -10,7 +10,19 @@ function getJokes(e) {
     xhr.onload = function() {
         if(this.status === 200) {
             const response = JSON.parse(this.responseText);
-            console.log(response);
+            
+            let output = '';
+
+            if(response.type === 'success') {
+                // as the api returns an object we need to loop within the value of the object, where the array is
+                response.value.forEach(function(joke) {
+                    output += `<li>${joke.joke}</li>`
+                }); 
+            } else {
+                output += '<li>Something went wrong</li>';
+            }
+
+            document.querySelector('.jokes').innerHTML = output;
         }
     }
 

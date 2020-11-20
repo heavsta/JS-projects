c78c1e78b64ef94817e515849832b95f9ae38491
heavsta <francoisleduc@ymail.com>
2020-11-20 17:14:18 +0100

    Fetch API for a json file done

--- Asynchronous JavaScript/Fetch API Sandbox/app.js
+++ Asynchronous JavaScript/Fetch API Sandbox/app.js
@@ -1,15 +1,40 @@
 document.getElementById('button1').addEventListener('click', getText);
 
+document.getElementById('button2').addEventListener('click', getJson);
+
+
+// Get local text file data
 function getText() {
     fetch('test.txt')
-    .then(function(res) {
-        return res.text();
-    })
-    .then(function(data) {
-        console.log(data);
-        document.getElementById('output').innerHTML = data;
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
+        .then(function(res) {
+            return res.text();
+        })
+        .then(function(data) {
+            console.log(data);
+            document.getElementById('output').innerHTML = data;
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+}
+
+
+// Get local json data
+function getJson() {
+    fetch('posts.json')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            let output = '';
+            data.forEach(function(post) {
+                output += `<li>${post.title}</li>
+                            <p>${post.body}</p>`
+            });
+            document.getElementById('output').innerHTML = output;
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 }

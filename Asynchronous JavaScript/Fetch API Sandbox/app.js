37fa07acd42d9cb67ae7c7c53bb9473c8e2055d8
heavsta <francoisleduc@ymail.com>
2020-11-20 17:24:44 +0100

    Fetch API for an external API done, used github users api as an example

--- Asynchronous JavaScript/Fetch API Sandbox/app.js
+++ Asynchronous JavaScript/Fetch API Sandbox/app.js
@@ -2,6 +2,7 @@ document.getElementById('button1').addEventListener('click', getText);
 
 document.getElementById('button2').addEventListener('click', getJson);
 
+document.getElementById('button3').addEventListener('click', getExternal);
 
 // Get local text file data
 function getText() {
@@ -31,6 +32,26 @@ function getJson() {
             data.forEach(function(post) {
                 output += `<li>${post.title}</li>
                             <p>${post.body}</p>`
+            });
+            document.getElementById('output').innerHTML = output;
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+}
+
+// Get from an external API
+function getExternal() {
+    fetch('https://api.github.com/users')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            let output = '';
+            data.forEach(function(user) {
+                output += `<li>${user.login}</li>
+                            <div class="userImg"><img src="${user.avatar_url}" alt="Github user avatar" width="300" height="300"</div>`
             });
             document.getElementById('output').innerHTML = output;
         })
